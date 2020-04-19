60bb76f1d3a6af91b7c5c9c76db8e60ce6ed4a14
eugenyly <eugeny@cryptaldash.com>
2020-04-19 20:10:08 +0300

    +add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,7 +12,19 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
-
+      fs.createReadStream(filepath)
+        .on('error', err => {
+          if (path.dirname(filepath) !== path.join(__dirname, 'files')) {
+            res.statusCode = 400;
+            res.end('Nested directories are not supported');
+          } else if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('not found');
+          } else {
+            res.statusCode = 500;
+            res.end('internal error');
+          }
+        }).pipe(res);
       break;
 
     default:
